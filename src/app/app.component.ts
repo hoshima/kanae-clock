45314bb29c95741abe0fa817028bfb010387e098
hoshima <akorihami@gmail.com>
2019-08-06 21:28:13 +0900

    display clock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'kanae-clock';
+
+  date$: Date;
+
+  ngOnInit() {
+    interval(1000).subscribe(x => this.date$ = new Date());
+  }
 }
